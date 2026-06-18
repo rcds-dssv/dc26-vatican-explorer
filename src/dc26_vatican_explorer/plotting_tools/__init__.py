f643e51c9513763688d9eea86a8cd8aac20ce37b
Emilio Lehoucq <emiliolehoucq@gmail.com>
2026-06-18 10:42:58 -0500

    exposing functions

---
+++ src/dc26_vatican_explorer/plotting_tools/__init__.py
@@ -0,0 +1,23 @@
+"""Agent-facing plotting helpers for Vatican Explorer analyses."""
+
+from dc26_vatican_explorer.plotting_tools.plotting_functions import (
+    create_bar_chart,
+    create_box_plot,
+    create_heatmap,
+    create_histogram,
+    create_line_chart,
+    create_ranked_bar_chart,
+    create_scatterplot,
+    save_figure,
+)
+
+__all__ = [
+    "create_bar_chart",
+    "create_box_plot",
+    "create_heatmap",
+    "create_histogram",
+    "create_line_chart",
+    "create_ranked_bar_chart",
+    "create_scatterplot",
+    "save_figure",
+]
